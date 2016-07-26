f6c9e3338943e260cbdeffb60f228d3c649bd948
Saurav Kothari <sauravkothari2@gmail.com>
2016-07-26 15:47:47 +0530

    First commit

---
+++ a.js
@@ -0,0 +1,35 @@
+var http = require('http');
+
+var options = {
+  host: 'localhost',
+  port: '45691',
+  path: '/check',
+  method: 'GET'
+}
+
+
+var req = http.request(options, function(res){
+  var response = "";
+  
+  res.setEncoding('utf8');
+  
+  res.on('data', function(chunk){
+    response += chunk.toString();
+  });
+  
+  res.on('end', function(){
+    console.log(response);
+    console.log(response);
+    console.log(response);
+    console.log(response);
+    console.log(response);
+    console.log(response);
+    console.log(response);
+    console.log(response);
+    console.log(response);
+    console.log(response);
+    console.log(response);
+  })
+});
+
+req.end();
